8c85e52789a4dd0114b8cd718ee4c6387e50de88
yzhng223 <yzhng223@illinois.edu>
2020-04-19 16:36:37 -0400

    Add Preference doc

--- recommender.py
+++ recommender.py
@@ -5,11 +5,18 @@ from typing import Dict, List
 class Preference:
 
     def __init__(self,
-                 preferred_datetimes: datetime,
+                 preferred_datetimes: List[datetime],
                  preferred_zipcode: int,
                  extras: str,
-                 # an optional weight for each preference as a tie breaker
                  weight: int = 0):
+        """
+        :param preferred_datetimes: preferred list of date + time in the order
+                                    of most preferred to least preferred
+        :param preferred_zipcode: preferred areas ex. 10044
+        :param extras: extra preferences in natural language
+        :param weight: an optional weight for tie breakers. higher weight can
+                       be taken into more consideration in all preferences
+        """
         self.preferred_datetimes = preferred_datetimes
         self.preferred_zipcode = preferred_zipcode
         self.extras = extras
@@ -20,7 +27,7 @@ def get_recommendation(movies: Dict[str, Dict],
                        preferences: Dict[str, Preference]) -> List[str]:
     """
     :param movies: movie database as <movie_id, movie_metadata>
-    :param preference: collected preferences as <user_id, preference>
+    :param preferences: collected preferences as <user_id, preference>
     :return: a list of movie_id ordered from most recommended to least recommended
     """
     recommendations = []
